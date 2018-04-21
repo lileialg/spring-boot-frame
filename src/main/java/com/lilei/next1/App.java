0dc4839b90934254d97ab1c2a8896a929fbc2ea9
lilei <lilei@cennavi.com.cn>
2018-04-21 11:54:56 +0800

    添加跨域访问

--- src/main/java/com/lilei/next1/App.java
+++ src/main/java/com/lilei/next1/App.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 
@@ -13,6 +17,20 @@ public class App extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 
 		SpringApplication.run(App.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		
+		return new WebMvcConfigurerAdapter() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+			
+			
+		};
 	}
 
 }
